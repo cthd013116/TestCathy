d5c84043589373a06f236fe8ce9cd5d90945e5a7
GARYWANGNB\cthd0 <cthd013116@gmail.com>
2025-01-09 22:47:29 +0800

    Add Project

---
+++ CathyTest2025/Controllers/CoinDeskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CathyTest2025.Controllers
+{
+   
+    public class CoinDeskController : Controller
+    {
+        public readonly ILogger _logger;
+
+        public CoinDeskController(ILogger<CoinDeskController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string CoinDeskAPI()
+        {
+            string result = "{\"time\":{\"updated\":\"Jan 9, 2025 06:59:23 UTC\",\"updatedISO\":\"2025-01-09T06:59:23+00:00\",\"updateduk\":\"Jan 9, 2025 at 06:59 GMT\"},\"disclaimer\":\"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org\",\"chartName\":\"Bitcoin\",\"bpi\":{\"USD\":{\"code\":\"USD\",\"symbol\":\"&#36;\",\"rate\":\"94,988.721\",\"description\":\"United States Dollar\",\"rate_float\":94988.7209},\"GBP\":{\"code\":\"GBP\",\"symbol\":\"&pound;\",\"rate\":\"75,760.629\",\"description\":\"British Pound Sterling\",\"rate_float\":75760.6291},\"EUR\":{\"code\":\"EUR\",\"symbol\":\"&euro;\",\"rate\":\"91,353.883\",\"description\":\"Euro\",\"rate_float\":91353.8825}}}";
+            return result;
+        }
+
+    }
+}
---
+++ CathyTest2025/Model/CoinDeskApiModel.cs
@@ -0,0 +1,83 @@
+﻿namespace CathyTest2025.Model
+{
+    public class CoinDeskApiModel
+    {
+        public Time _Time { get; set; }
+
+        /// <summary>
+        /// 免責聲明
+        /// </summary>
+        public string disclaimer { get; set; }
+
+        /// <summary>
+        /// 圖表名稱
+        /// </summary>
+        public string chartName { get; set; }
+
+
+
+    }
+    public class Time
+    { 
+        /// <summary>
+        /// 更新時間_UTC
+        /// </summary>
+        public string updated { get; set; }
+
+        /// <summary>
+        /// 更新時間_ISO
+        /// </summary>
+        public string updatedISO { get; set; }
+
+        /// <summary>
+        /// 更新時間_UK
+        /// </summary>
+        public string updateduk { get; set; }
+    }
+
+    public class bpi
+    { 
+        /// <summary>
+        /// 美金
+        /// </summary>
+        public CurrencyInfo USD { get; set; }
+
+        /// <summary>
+        /// 英鎊
+        /// </summary>
+        public CurrencyInfo GBP { get; set; }
+
+        /// <summary>
+        /// 歐元
+        /// </summary>
+        public CurrencyInfo EUR { get; set; }
+
+    }
+    public class CurrencyInfo
+    { 
+        /// <summary>
+        /// 幣別代碼
+        /// </summary>
+        public string code { get; set; }
+
+        /// <summary>
+        /// 幣別象徵
+        /// </summary>
+        public string symbol { get; set; }
+
+        /// <summary>
+        /// 匯率
+        /// </summary>
+        public string rate { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string description { get; set; }
+
+        /// <summary>
+        /// 詳細匯率
+        /// </summary>
+        public string rate_float { get; set; }
+    }
+}
